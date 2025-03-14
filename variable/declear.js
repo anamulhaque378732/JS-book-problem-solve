f5b75943bf8ca119b221c7c00ec253fd0b617191
Anamul Haque <anamulhaque4631@gmail.com>
2025-03-14 21:43:59 +0600

    variable

---
+++ variable/declear.js
@@ -0,0 +1,52 @@
+const birthYear = 1997;
+const mySisAndBRo = 2;
+const students = 56;
+const mark = 65;
+const room = 4;
+const readingTime = 4;
+const total = 0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
+//console.log(total);
+const myAge = 24;
+//console.log(myAge);
+const booksToRead= 20;
+//console.log(20);
+const workUpTime = 9;
+const wastedTime = 8;
+const borrow=2500;
+const goal = 4;
+const pillow = 2;
+//console.log(wastedTime,workUpTime,borrow,goal,pillow);
+const name ="anamul";
+const address = "dhunat , bogura";
+const clg= "tmss";
+const fan = "shruti hasan";
+const movieOfTheYear = "snoden";
+const hobby = "coading";
+const color ="block";
+
+const babbu = true;
+const isSentMessage = false;
+const celebrating = true;
+const isHappy = true;
+const hasMoney =false;
+const isHungry = true;
+const isStudyDone = false;
+const isPhoneCharged = true;
+const isDinnerDone =true; 
+console.log(isDinnerDone);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ variable/math.js
@@ -0,0 +1,27 @@
+const eran = 100;
+const cost = 40;
+//console.log(eran-cost);
+const myPen = 10;
+const use = 2;
+//console.log(myPen/use);
+const myMoney = 60;
+const eachTimeCost = 15;
+//console.log(myMoney/eachTimeCost);
+const hello = "hello";
+const world = "world";
+//console.log(hello + world);
+const pulse = 261;
+const rice = 153;
+//console.log(pulse+rice);
+
+const money = 500;
+const eachTimeCost1 = 75;
+//console.log(money / eachTimeCost1, money % eachTimeCost1);
+// console.log(8 % 2);
+// console.log(50 % 9);
+// console.log("bangla"+"desh");
+
+const totalRice = 480;
+const eachDay = 4;
+const parMonth = 4 * 30;
+console.log(totalRice / eachDay, totalRice / parMonth);
