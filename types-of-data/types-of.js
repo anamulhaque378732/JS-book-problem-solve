97e1ae16bd70540e3c9af237f116b328605cc53a
Anamul Haque <anamulhaque4631@gmail.com>
2025-08-06 10:08:04 +0600

    types of dada complate

--- types-of-data/types-of.js
+++ types-of-data/types-of.js
@@ -5,3 +5,51 @@ const temperature = 37.5;
 const isHungry = false;
 const isTired = true;
 // console.log(typeof price,typeof learning,typeof megaCiy,typeof temperature,typeof isHungry,typeof isTired,typeof temperature, typeof isHungry, typeof isTired, typeof isHungry);
+// let price = 45;
+// price = 35;
+// let age = 24;
+// age = 26;
+let pocketMoney = 500;
+pocketMoney = 150;
+//console.log(pocketMoney);
+let StudyTime = 4;
+StudyTime = 0;
+// console.log(StudyTime);
+const fevoriteFood = 'hash';
+const city = 'dhaka';
+const City = "chittagong";
+const userName = "jhon";
+const username = "anamul";
+const myHomeAddress = "38 carmaicel road , bogura";
+// const super= 18;
+//const 2ndPosition = "tamim";// variable cannot declare 1 st letter by number
+const fatherAge = parseInt('50');
+const sonAge = parseInt('20');
+//console.log(typeof parseInt(fatherAge + sonAge));
+// console.log(fatherAge + sonAge);
+
+const age = parseInt('50f');
+const weight = parseInt('9twenty');
+const address = parseInt('tom60'
+);
+//console.log(age, weight, address);
+const sugar = parseInt('1.9622222222');
+// console.log(sugar);
+const sugar2 = parseFloat('1.020223023');
+//console.log(sugar2);
+const first = 12.2521522;
+const second = 31.2525555;
+const total = first + second;
+//console.log(parseFloat(total.toFixed(5)));
+const num = "20";
+//console.log(parseInt(num) + 10);
+//console.log(parseInt('3.141632'));
+//console.log(parseInt('premikBoy'));
+const num2 = 2.01421 + 44.2555;
+
+//console.log(parseFloat(num2.toFixed(2)));
+const num3 = parseFloat('56.78');
+const num4 = parseFloat('12.36');
+console.log(num3 + num4);
+
+
