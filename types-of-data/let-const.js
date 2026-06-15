2d42c84c14cecac88c8e37640eac729397bf145a
Anamul Haque <anamulhaque4631@gmail.com>
2026-06-14 21:16:44 -0700

    Add new data

--- types-of-data/let-const.js
+++ types-of-data/let-const.js
@@ -1,23 +1,19 @@
-let age=5;
-age=21;
-let pocketMoney=100;
-pocketMoney=200;
-let studyTime=5;
-studyTime=10;
-let friends=5;
-friends=10;
-let phonePrice=5000;
-phonePrice=8000;
-let chairsCount=5;
-chairsCount=12;
-let playTime=5;
-playTime=0;
+let age = 5;
+age = 21;
+let pocketMoney = 100;
+pocketMoney = 200;
+let studyTime = 5;
+studyTime = 10;
+let friends = 5;
+friends = 10;
+let phonePrice = 5000;
+phonePrice = 8000;
+let chairsCount = 5;
+chairsCount = 12;
+let playTime = 5;
+playTime = 0;
 //console.log(age,pocketMoney,studyTime,friends,phonePrice,chairsCount,playTime);
 
-
-
-
-
-
-
-
+let name = "Anamul";
+name = "Haque";
+let city = "Dhaka";
